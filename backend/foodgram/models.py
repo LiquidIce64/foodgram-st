a76ea35fdad97b9c3b0a88329adf69bfa4634942
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-06-10 17:41:07 +0300

    added models

---
+++ backend/foodgram/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    measurement_unit = models.CharField(max_length=64)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes')
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='recipes/')
+    cooking_time = models.IntegerField()
+    text = models.TextField()
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    amount = models.IntegerField()
+
+
+class ShoppingCartItem(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='shopping_cart_items')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='shopping_cart_items')
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='favorites')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='favorites')
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='subscriptions')
+    subscribed_to = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='subscribers')
