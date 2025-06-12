9f4278a12d9fccbb34d858fad157f11a9c8a4968
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-06-13 02:00:54 +0300

    changed ingredient view permissions

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.viewsets import (
     ModelViewSet, ReadOnlyModelViewSet,
@@ -57,6 +58,7 @@ class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = models.Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
     filter_backends = (filters.NameSearchFilter,)
+    permission_classes = (AllowAny,)
 
 
 class RecipeViewSet(ModelViewSet):
