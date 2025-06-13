2a3084c2c7420e4b9e9b84e0cde9645e85c10e68
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-06-13 17:02:30 +0300

    edited admin models and added localisation

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -2,9 +2,62 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Profile)
-admin.site.register(models.Subscription)
-admin.site.register(models.Ingredient)
-admin.site.register(models.Recipe)
-admin.site.register(models.Favorite)
-admin.site.register(models.ShoppingCartItem)
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ['username', 'email', 'avatar']
+    fieldsets = [(None, {'fields': ['avatar']})]
+    search_fields = ('user__username', 'user__email')
+
+    @admin.display(description='Имя пользователя')
+    def username(self, obj):
+        return obj.user.username
+
+    @admin.display(description='Адрес электронной почты')
+    def email(self, obj):
+        return obj.user.email
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(models.Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subscribed_to']
+    search_fields = ('user__username', 'subscribed_to__username')
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'measurement_unit']
+    search_fields = ('name',)
+
+
+@admin.register(models.RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ['recipe', 'ingredient', 'amount', 'measurement_unit']
+    search_fields = ('recipe__name', 'ingredient__name')
+
+    @admin.display(description='Единица измерения')
+    def measurement_unit(self, obj):
+        return obj.ingredient.measurement_unit
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author_name']
+    search_fields = ('name', 'author__username')
+
+    @admin.display(description='Автор')
+    def author_name(self, obj):
+        return obj.author.username
+
+
+@admin.register(models.Favorite, models.ShoppingCartItem)
+class UserRecipeListAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user', 'recipe']
+    search_fields = ('user__username', 'recipe__name')
--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -8,8 +8,15 @@ User = get_user_model()
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile')
-    avatar = models.ImageField(upload_to='users/', null=True, blank=True)
+        User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
+    avatar = models.ImageField(upload_to='users/', null=True, blank=True, verbose_name='Аватар')
+
+    class Meta:
+        verbose_name = 'профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
 
 
 @receiver(models.signals.post_save, sender=User)
@@ -20,43 +27,85 @@ def create_profile(sender, instance, created, **kwargs):
 
 class Subscription(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='subscriptions')
+        User, on_delete=models.CASCADE, related_name='subscriptions', verbose_name='Пользователь')
     subscribed_to = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='subscribers')
+        User, on_delete=models.CASCADE, related_name='subscribers', verbose_name='Подписан на')
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f"Подписка {self.user.username} на {self.subscribed_to.username}"
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=128)
-    measurement_unit = models.CharField(max_length=64)
+    name = models.CharField(max_length=128, verbose_name='Название')
+    measurement_unit = models.CharField(max_length=64, verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='recipes')
-    name = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='recipes/')
-    cooking_time = models.IntegerField()
-    text = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+        User, on_delete=models.CASCADE, related_name='recipes', verbose_name='Автор')
+    name = models.CharField(max_length=256, verbose_name='Название')
+    image = models.ImageField(upload_to='recipes/', verbose_name='Изображение')
+    cooking_time = models.IntegerField(verbose_name='Время готовки (в минутах)')
+    text = models.TextField(verbose_name='Описание')
+    date_posted = models.DateTimeField(auto_now_add=True, verbose_name='Время публикации')
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
 
 
 class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredients')
+        Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredients', verbose_name='Ингредиент')
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='ingredients')
-    amount = models.IntegerField()
+        Recipe, on_delete=models.CASCADE, related_name='ingredients', verbose_name='Рецепт')
+    amount = models.IntegerField(verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецептов'
+
+    def __str__(self):
+        return f"{self.recipe.name} - {self.ingredient.name}"
 
 
 class ShoppingCartItem(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='shopping_cart')
+        User, on_delete=models.CASCADE, related_name='shopping_cart', verbose_name='Пользователь')
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='in_shopping_carts')
+        Recipe, on_delete=models.CASCADE, related_name='in_shopping_carts', verbose_name='Рецепт')
+
+    class Meta:
+        verbose_name = 'предмет корзины'
+        verbose_name_plural = 'Предметы корзин'
+
+    def __str__(self):
+        return f"Корзина {self.user.username} - {self.recipe.name}"
 
 
 class Favorite(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='favorites')
+        User, on_delete=models.CASCADE, related_name='favorites', verbose_name='Пользователь')
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='favorites')
+        Recipe, on_delete=models.CASCADE, related_name='favorites', verbose_name='Рецепт')
+
+    class Meta:
+        verbose_name = 'избранное'
+        verbose_name_plural = 'Избранные'
+
+    def __str__(self):
+        return f"Избранное {self.user.username} - {self.recipe.name}"
