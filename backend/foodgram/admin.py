60ddabb41423715be3360f554f072ae047554301
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-06-13 09:37:01 +0300

    added models into admin

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Profile)
+admin.site.register(models.Subscription)
+admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
+admin.site.register(models.Favorite)
+admin.site.register(models.ShoppingCartItem)
